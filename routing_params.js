50c42b51f3079fb4c30eb4a7e648dab581e169f5
pamuba <pamuba@users.noreply.github.com>
2020-06-28 01:57:37 +0530

    15th

--- routing_params.js
+++ routing_params.js
@@ -1,10 +1,38 @@
-var app = angular.module('app',['ngRoute'])
+var app = angular.module("app", ["ngRoute"]);
 
-app.config(['$routeProvider', function($routeProvider){
-    $routeProvider.when('/',{
-       template:"<strong>Welcole to the APP</strong>"
-    })
+app.config([
+  "$routeProvider",
+  function ($routeProvider) {
+    $routeProvider.when("/sumurl/:a/:b", {
+      templateUrl: 'sumurl.html',
+      controller:'sumurl'
+    });
+    $routeProvider.when("/suminput", {
+        templateUrl: 'suminput.html',
+        controller:'suminput'
+      });
+    $routeProvider.when("/", {
+      template: "<strong>Welcole to the APP</strong>",
+    });
     $routeProvider.otherwise({
-        template:"<strong>No Content Available</strong>"
-     })
-}])
+      template: "<strong>No Content Available</strong>",
+    });
+  },
+]);
+
+app.controller('sumurl',['$scope', '$routeParams', function($scope,$routeParams){
+    $scope.a = $routeParams.a;
+    $scope.b = $routeParams.b;
+}]);
+
+app.controller('suminput',['$scope','$location','$interpolate', function($scope,$location,$interpolate){
+    $scope.a = 0;
+    $scope.b = 0;
+
+    $scope.doSum = function() {
+        var url = $interpolate('/sumurl/{{a}}/{{b}}')($scope)
+        console.log(url)
+        $location.path(url);
+    }
+}]);
+
