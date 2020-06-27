58bcf6b04f88f648a61421cafd4d54b6a9bf0466
pamuba <pamuba@users.noreply.github.com>
2020-06-27 23:59:30 +0530

    7th

--- direc.js
+++ direc.js
@@ -1,5 +1,11 @@
 var app = angular.module("app", []);
 
 app.controller("emp", function ($scope, $q) {
-
+    $scope.msg = "This is message from directive"
 });
+
+app.directive("myInfoMsg", function(){
+    return{
+        templateUrl:"my-info-msg.html"
+    }
+})
