6c68a8fd82185219949de1aedc8432175fac788c
pamuba <pamuba@users.noreply.github.com>
2020-06-27 23:23:18 +0530

    third

--- scripts.js
+++ scripts.js
@@ -1,17 +1,33 @@
 var app = angular.module("app", []);
 
 app.controller("AppCtrl", function ($scope, $timeout) {
-  function add(x, y, callback) {
-    $timeout(function () {
-      callback(x + y);
+  function add(x, y) {
+    return $timeout(function () { //$timeout by default returns a promise
+      return (x + y);
     }, 100);
   }
 
   var startTime = Date.now();
-  add(5, 2, function (result) {
-    add(result, 3, function(result){
-      $scope.result = result;
-      $scope.elapsedTime = Date.now()-startTime;
-    })
-  });
+
+  //alternate way -- promise chaining
+  add(5,2)
+        .then(function(result){
+          return add(result, 3);//.then block always return a promise
+        })
+        .then(function(result){
+          return add(result, 1);
+        })
+        .then(function(result){
+          $scope.result = result;
+          $scope.elapsedTime = Date.now() - startTime;
+        })
+
+  // add(5, 2, function (result) {
+  //   add(result, 3, function (result) {
+  //     add(result, 1, function (result) {
+  //       $scope.result = result;
+  //       $scope.elapsedTime = Date.now() - startTime;
+  //     }, function(error){/**error handling */});
+  //   },function(error){/**error handling */});
+  // },function(error){/**error handling */});
 });
